04a7a2556fbb94ffab17cb98ef95a0825d5ef6b1
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-23 21:17:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,9 +3,11 @@ class Dog
     puts "Woof!"
   end
 
-  fido = Dog.new
-  fido.bark
-
-  def bark puts "The Dog is sitting"
+  def sit
+    puts "The Dog is sitting"
  end
 end
+
+fido = Dog.new
+fido.bark
+fido.sit
